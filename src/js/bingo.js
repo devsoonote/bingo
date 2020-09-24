1c6d49ece3a815b4c86c50097e387d81db133412
devsoonote <sookung93@gmail.com>
2020-09-25 02:42:01 +0900

    feature: update bingo test

--- src/js/bingo.js
+++ src/js/bingo.js
@@ -20,10 +20,33 @@ export default class Bingo {
     return this.history;
   }
 
+  activeClass(target) {
+    if(this.current.containerEl !== target.parentNode.parentNode.parentNode) {
+      alert('현재 차례가 아닙니다')
+      return
+    }
+
+    target.classList.add('active');
+    const boards = document.getElementsByClassName('board');
+    const otherBoard = [].reduce.call(boards, (acc, cur) => cur !== target.parentNode.parentNode ? cur : acc);
+    this.addClassOfOtherBoard(otherBoard, target);
+  }
+
+  addClassOfOtherBoard(boardEl, target){
+    Array.prototype.forEach.call(boardEl.children, (trEl) => {
+      Array.prototype.forEach.call(trEl.children, (tdEl) => {
+        if (tdEl.textContent === target.textContent) {
+          tdEl.classList.add('active');
+        }
+      })
+    })
+  }
+
   updateHistoryView(historyBoxEl, target) {
     if (!historyBoxEl) throw Error('historyBoxEl가 주입되지 않았습니다');
     if (!target) throw Error('target이 주입되지 않았습니다');
 
+    this.activeClass(target);
     this.activeHistory(target);
     historyBoxEl.insertAdjacentHTML('beforeend', `<span>${this.history[this.history.length - 1]}</span>`)
   }
--- src/js/bingo.spec.js
+++ src/js/bingo.spec.js
@@ -1,26 +1,54 @@
 import Bingo from "./bingo";
 import Player from "./player";
+import CreateBoardUI from "./createBoardUI";
 
 describe('Bingo', () => {
-  let bingo, me, you, target, historyBoxEl;
+  let bingo, me, you, target, historyBoxEl, meEl, youEl;
 
   beforeEach(() => {
-    const meEl = document.createElement('div');
-    const youEl = document.createElement('div');
-    me = new Player(meEl, {row: 5, cell: 5, name: 'test1'});
-    you = new Player(youEl, {row: 5, cell: 5, name: 'test2'});
+    meEl = $('<div class="game-board" id="me">' +
+          '<h3 class="game-board-name" id="me-name-box">Me</h3>' +
+            '<table class="board" id="me-board"></table>' +
+            '<div class="score-container">' +
+              '<span>빙고: </span>' +
+              '<span id="me-score" class="score-text">3</span>' +
+            '</div>' +
+        '</div>');
+    youEl = $('<div class="game-board" id="you">' +
+            '<h3 class="game-board-name" id="you-name-box">You</h3>' +
+              '<table class="board" id="you-board"></table>' +
+              '<div class="score-container">' +
+                '<span>빙고: </span>' +
+                '<span id="me-score" class="score-text">3</span>' +
+              '</div>' +
+        '</div>');
+
+    $('body').append(youEl);
+    $('body').append(meEl);
+
+    const meId = document.querySelector('#me');
+    const youId = document.querySelector('#you');
+
+    me = new Player(meId, {row: 5, cell: 5, name: 'test1'});
+    you = new Player(youId, {row: 5, cell: 5, name: 'test2'});
     bingo = new Bingo([me, you]);
+    const callback = function(target) {}
+    new CreateBoardUI(callback, me);
+    new CreateBoardUI(callback, you);
 
-    target = document.createElement('span');
     historyBoxEl = document.createElement('div');
-    target.innerHTML = '1';
-
+    target = meId.querySelector('td');
   });
+
+  afterEach(() => {
+    meEl.remove();
+    youEl.remove();
+  })
 
   describe('activeHistory()', () => {
     it('history 배열에 클릭 값이 추가된다', () => {
       bingo.activeHistory(target);
-      expect(bingo.history).toEqual(['1']);
+      expect(bingo.history).toEqual([target.textContent]);
     });
 
     it('changeCurrent()가 호출된다', () => {
@@ -44,6 +72,13 @@ describe('Bingo', () => {
       expect(bingo.activeHistory).toHaveBeenCalled();
     });
 
+    // 현재 차례를 정하지 않음... 이거 변경하기
+    it('activeClass()가 호출된다', () => {
+      spyOn(bingo, 'activeClass');
+      bingo.updateHistoryView(historyBoxEl, target);
+      expect(bingo.activeClass).toHaveBeenCalled();
+    });
+
     it('history의 내용이 출력된다', () => {
       bingo.updateHistoryView(historyBoxEl, target);
       expect(historyBoxEl.lastChild.textContent).toBe(target.textContent);
@@ -61,4 +96,13 @@ describe('Bingo', () => {
       expect(actual).toThrowError()
     });
   });
+
+  describe('activeClass()', () => {
+    it('addClassOfOtherBoard() 함수가 실행된다', () => {
+      spyOn(bingo, 'addClassOfOtherBoard');
+      bingo.activeClass(target);
+
+      expect(bingo.addClassOfOtherBoard).toHaveBeenCalled();
+    })
+  })
 });
